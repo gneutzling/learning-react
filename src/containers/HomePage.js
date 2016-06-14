921231ee09ea8cd270cc26e39c0f24e06c38c4ea
Gabriel Neutzling <gabrielneutzling@gmail.com>
2016-06-14 14:41:31 -0300

    Add initial files based on redux-boilerplate by @chentsulin.

---
+++ src/containers/HomePage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Learning React</h1>
+      </div>
+    );
+  }
+}
+
+
+export default HomePage;
